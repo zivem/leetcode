5f1826b9c4ef6ec63332add0aaa13e24bdb085ee
lingchening <1101012296@qq.com>
2018-04-03 15:55:29 +0800

    4 Median of Two Sorted Arrays

--- src/question1_10/_4_MedianofTwoSortedArrays.java
+++ src/question1_10/_4_MedianofTwoSortedArrays.java
@@ -131,49 +131,51 @@ package question1_10;
 public class _4_MedianofTwoSortedArrays {
     public static void main(String[] args){
         int[] nums1 = {1,2};
-        int[] nums2 = new int[]{3,4};
+        int[] nums2 = new int[]{3,4,5};
         double result = findMedianSortedArrays(nums1,nums2);
         System.out.println(result);
     }
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length;
         int n = nums2.length;
-        if(m > n){
-            return findMedianSortedArrays(nums2, nums1);
+        if (m > n) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+            int tmp = m;
+            m = n;
+            n = tmp;
         }
-        if(n == 0){
-            return 0.0;
-        }
-        int imin=0;
+        int imin = 0;
         int imax = m;
-        int half = (m+n+1)/2;
-        while (imin <= imax){
-            int i = (imin + imax)/2;
+        int half = (m + n + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
             int j = half - i;
-            if(i < m && nums2[j-1] > nums1[i]){
-                imin = i + 1;
-            }else if (i >0 && nums1[i-1] > nums2[j]){
-                imax = i - 1;
-            }else{
-                int maxLeft =0,minRight=0;
-                if (i==0){
-                    maxLeft =nums2[j-1];
-                }else if(j==0){
-                    maxLeft = nums1[i-1];
-                }else{
-                    maxLeft = Math.max(nums1[i-1],nums2[j-1]);
+            if (i < imax && nums2[j - 1] > nums1[i]) {
+                imin = imin + 1;
+            } else if (i > imin && nums1[i - 1] > nums2[j]) {
+                imax = imax - 1;
+            } else {
+                int maxLeft = 0, minRight = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
-                if(m+n%2==1){
+                if ((m + n) % 2 == 1) {
                     return maxLeft;
                 }
-                if(i==m){
-                    minRight =nums2[j];
-                }else if(j==n){
-                    minRight =nums1[i];
-                }else{
-                    minRight = Math.min(nums1[i],nums2[j]);
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums1[i], nums2[j]);
                 }
-                return (maxLeft+minRight)/2.0;
+                return (maxLeft + minRight) / 2.0;
             }
         }
         return 0.0;
