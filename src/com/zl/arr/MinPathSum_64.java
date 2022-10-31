6213d4f62320371c3776143481bbd53e351a256a
lei.zz <1101012296@qq.com>
2022-10-31 22:40:52 +0800

    leetcode 64

--- src/com/zl/arr/MinPathSum_64.java
+++ src/com/zl/arr/MinPathSum_64.java
@@ -5,19 +5,18 @@ package com.zl.arr;
  */
 public class MinPathSum_64 {
     public static void main(String[] args) {
-        int[][] grid = {{1,3,1},{1,5,1},{4,2,1}};
+        int[][] grid = {{1, 3, 1}, {1, 5, 1}, {4, 2, 1}};
         System.out.println(minPathSum(grid));
     }
 
     public static int minPathSum(int[][] grid) {
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(i == 0 && j == 0){
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
                     continue;
-                }
-                else if(i == 0)  {
+                } else if (i == 0) {
                     grid[i][j] = grid[i][j - 1] + grid[i][j];
-                } else if(j == 0) {
+                } else if (j == 0) {
                     grid[i][j] = grid[i - 1][j] + grid[i][j];
                 } else {
                     grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
@@ -25,5 +24,28 @@ public class MinPathSum_64 {
             }
         }
         return grid[grid.length - 1][grid[0].length - 1];
+    }
+
+    public static int minPathSum1(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        //降维，降维一维数组
+        int[] dp = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[j] = dp[j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[j] = dp[j] + grid[i][j];
+                } else {
+                    dp[j] = Math.min(dp[j - 1], dp[j]) + grid[i][j];
+                }
+            }
+
+        }
+        return dp[m - 1];
     }
 }
