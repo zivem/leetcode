8212856be9eac19fec2361e2079b66d6a95d720b
lei.zz <1101012296@qq.com>
2022-11-13 09:04:48 +0800

    happy num

---
+++ src/com/zl/other/HappyNum.java
@@ -0,0 +1,34 @@
+package com.zl.other;
+
+public class HappyNum {
+    public static void main(String[] args) {
+        boolean happy = isHappy(19);
+        System.out.println(happy);
+    }
+
+
+
+    public static boolean isHappy(int n) {
+        int slow = n, fast = n;
+        slow = bitSum(slow);
+        fast = bitSum(fast);
+        fast = bitSum(fast);
+        while (slow != fast) {
+            slow = bitSum(slow);
+            fast = bitSum(fast);
+            fast = bitSum(fast);
+        }
+        return slow == 1;
+    }
+
+    public static int bitSum(int num) {
+        int sum = 0;
+        while (num > 0) {
+            int b = num % 10;
+            sum += b * b;
+            num /= 10;
+        }
+
+        return sum;
+    }
+}
