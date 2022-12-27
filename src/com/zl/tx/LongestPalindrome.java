beb650ebe1b459baa70766e23d1a5aea444bd485
lei.zz <1101012296@qq.com>
2022-12-27 08:16:52 +0800

    leetcode 409

---
+++ src/com/zl/tx/LongestPalindrome.java
@@ -0,0 +1,16 @@
+package com.zl.tx;
+
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+        // 找出可以构成最长回文串的长度
+        int[] arr = new int[128];
+        for(char c : s.toCharArray()) {
+            arr[c]++;
+        }
+        int count = 0;
+        for (int i : arr) {
+            count += (i % 2);
+        }
+        return count == 0 ? s.length() : (s.length() - count + 1);
+    }
+}
